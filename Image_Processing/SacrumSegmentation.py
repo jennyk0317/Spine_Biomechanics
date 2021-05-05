0c6b064861d43c582e5786be1a30a09ac833ab03
Kim  Ji Hyun <jikimji@ethz.ch>
2021-05-05 17:55:31 +0200

    Active Contour

--- Image_Processing/SacrumSegmentation.py
+++ Image_Processing/SacrumSegmentation.py
@@ -5,17 +5,16 @@ from scipy.io import loadmat
 from skimage.segmentation import active_contour
 from skimage.filters import gaussian
 from skimage.measure import find_contours
+from skimage.exposure import histogram
+from skimage.transform import rescale, resize
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import cv2 as cv
 
 
 # %%
-os.chdir(r'C:\Users\jihyk\Desktop\SBM\MATLAB\Spine annotation\EOS Spine Images\Spine_EOS_0050-0103\0051')
-mat = loadmat('0051 marked.mat')
-
-# %%
-os.chdir(r'C:\Users\jihyk\Desktop\SBM')
+#os.chdir(r'C:\Users\jihyk\Desktop\SBM')
 img = imread('sacrum_sagital.jpg')
 img = rgb2gray(img)
 mask = imread('sacrum_sagital_mask.jpg')
@@ -43,4 +42,62 @@ line = contours[1]
 imshow(img)
 plt.plot(line[:,1], line[:,0])
 plt.show()
+# %%
+#print(np.shape(line))
+
+im = cv.imread('sacrum_sagital.jpg')
+imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+mask = imread('sacrum_sagital_mask.jpg')
+maskgray = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
+
+ret, thresh = cv.threshold(maskgray, 127, 255, 0)
+contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+cnt = contours[3]
+cnt_scaled = scale_contour(cnt, 2)
+cv.drawContours(mask, [cnt_scaled], 0, (0,255,0), 3)
+#cv.drawContours(mask, [cnt], 0, (0,255,0), 3)
+cv.imshow('image', im)
+cv.imshow('Contours', mask)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+print("Number of Contours found = " + str(len(contours)))
+
+init1 = cnt_scaled[:, :,0]
+init2 = cnt_scaled[:, :,1]
+init = np.concatenate((init1, init2), axis = 1)
+print(np.shape(init))
+print("Size of contour:", np.shape(cnt_scaled))
+
+
+
+# %%
+imshow(im)
+
+plt.plot(cnt_scaled[:, :, 0], cnt_scaled[:, :, 1])
+plt.show()
+# %%
+def scale_contour(cnt, scale):
+    M = cv.moments(cnt)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+
+    cnt_norm = cnt - [cx, cy]
+    cnt_scaled = cnt_norm * scale
+    cnt_scaled = cnt_scaled + [cx, cy]
+    cnt_scaled = cnt_scaled.astype(np.int32)
+
+    return cnt_scaled
+
+# %%
+snake = active_contour(gaussian(im, 3),
+                       cnt_scaled, alpha=0.015, beta=10, gamma=0.001)
+
+# %%
+fig, ax = plt.subplots(figsize=(7, 7))
+ax.imshow(img, cmap=plt.cm.gray)
+ax.plot(init[:, 1], init[:, 0], '--r', lw=3)
+ax.plot(snake[:, 1], snake[:, 0], '-b', lw=3)
+ax.set_xticks([]), ax.set_yticks([])
+ax.axis([0, img.shape[1], img.shape[0], 0])
 # %%
